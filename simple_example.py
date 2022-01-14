0d273178d12e98407b11c4132addeeede314d0ab
Markus <mmiller96@uni-bremen.de>
2022-01-14 12:00:43 +0100

    create dataset

--- simple_example.py
+++ simple_example.py
@@ -1,1 +1,11 @@
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
+import pyro
+
+x1 = np.random.normal(2, 0.3, 70)
+x2 = np.random.normal(6, 1, 150)
+x = np.hstack((x1, x2))
+
+plt.hist(x, bins=40)
+plt.grid()
+plt.show()
